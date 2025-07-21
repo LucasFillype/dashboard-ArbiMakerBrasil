cd8156f96be2364fe987830d96e2881bd72ccb02
lucas fillype <lucasfillype00@gmail.com>
2025-07-21 01:39:40 -0300

    note page initial commit

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,1 +1,16 @@
 import { Textarea } from "@/components/ui/textarea";
+
+
+export default function NotesPage (){
+    return(
+    <main>
+        <header>
+            <h1 className="text-bold text-white flex justify-center mb-[30px]"> Digite Suas Anotações na Área abaixo!</h1>
+        </header>
+        <section className="flex justify-center">
+            <Textarea className=" w-[300px] bg-zinc-800 border-zinc-700 text-white"/>
+        </section>
+
+    </main>
+    )
+}
