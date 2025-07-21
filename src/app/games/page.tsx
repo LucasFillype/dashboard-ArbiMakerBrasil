c0fe3ff7bc17cf004dc13ac8d7656876ef79f532
lucas fillype <lucasfillype00@gmail.com>
2025-07-21 01:09:44 -0300

    data att commit

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -2,19 +2,7 @@ import { columns, Games } from "./columns"
 import { GamesTable } from "./data-table"
 
 async function getData(): Promise<Games[]> {
-
-  return [
-    {
-      Data: "",
-      Horário: "",
-      NumeroDaAposta: "",
-      Stake: "",
-      status: "Em aberto",
-      Casas: "",
-      ODD: "",
-    },
-
-  ]
+  return []; 
 }
 
 export default async function GamesPage() {
